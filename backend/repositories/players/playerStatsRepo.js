abb56c1ff7e92ce810b2adc1cc7ed58c47ba9bf7
riyu.iring <oribelloruthiemy@gmail.com>
2026-04-23 19:39:23 +0800

    Add: add new player stats routes and controller

--- backend/repositories/players/playerStatsRepo.js
+++ backend/repositories/players/playerStatsRepo.js
@@ -7,3 +7,32 @@ export async function findAll() {
 export async function findByMatch(matchId) {
   return await sql`SELECT * FROM player_stats WHERE match_id = ${matchId}`;
 }
+
+export async function findByPlayer(playerId) {
+  return await sql`SELECT * FROM player_stats WHERE player_id = ${playerId}`;
+}
+
+export async function findByMatchAndPlayer(matchId, playerId) {
+  return await sql`SELECT * FROM player_stats WHERE match_id = ${matchId} AND player_id = ${playerId}`;
+}
+
+export async function create(data) {
+  return await sql`INSERT INTO player_stats ${sql(data, 'player_id', 'match_id', 'stats_id', 'set_number', 'value')}`;
+}
+
+export async function update(id, data) {
+  return await sql`
+    UPDATE player_stats
+    SET ${sql(data, 'value')}
+    WHERE entry_id = ${id}
+    RETURNING *
+  `;
+}
+
+export async function deleteRecord(id) {
+  return await sql`
+    DELETE FROM player_stats
+    WHERE entry_id = ${id}
+    RETURNING *
+  `;
+}
